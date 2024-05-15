e5c51a8f49886c42d04be4d32e38a9efced937de
Prateek <74645710+dicedealer@users.noreply.github.com>
2024-05-15 13:15:45 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,19 @@
+## Note that we will compile this to test.exe
+
+#include <windows.h>
+#include <stdio.h>
+
+int main (void)
+{
+    HINSTANCE hDLL;
+    //Loading DLL
+    hDLL = LoadLibrary(TEXT("example.dll"));
+
+    //Check whether DLL was Loaded or not loaded
+    if (hDLL != NULL){
+            printf("DLL Found\n");
+    } else {
+            print("DLL Not Found\n");
+    }
+return 0;
+}
