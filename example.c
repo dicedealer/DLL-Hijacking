f6120da21355f811173ac9fb02bddf096369bfa9
Prateek <74645710+dicedealer@users.noreply.github.com>
2024-05-15 12:59:58 +0530

    Create example.c

---
+++ example.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+
+BOOL WINAPI
+DllMain (Handle hDll, DWORD dwReason, LPVOID lpReserved)
+
+{ 
+switch (dwReason){
+  case DLL_PROCESS_ATTACH:
+    MessageBox(NULL, //Owner
+                "The DLL has been loaded", //Message
+                "COMPROMISED!!", //Title
+                MB_ICONERROR | MB_OK //TYPE
+                );
+break;
+}
+return TRUE;
+}
